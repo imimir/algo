19aa84920db73a2d51a456e281c9e2851193f03c
mimir <scarletfores3@gmail.com>
2017-05-04 22:33:01 +0300

    Add lru_cache support, change syntax to python 3.

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,10 @@
-def memo(f):
-    cache = {}
-
-    def inner(n):
-        if n not in cache:
-            cache[n] = f(n)
-        return cache[n]
-
-    return inner
+from functools import lru_cache
 
 def fibonacci(n):
     assert n >= 0
     return n if n < 2 else fibonacci(n - 1) + fibonacci(n - 2)
 
-fibonacci = memo(fibonacci)
+fibonacci = lru_cache(maxsize = None)(fibonacci)
 
 for i in range(100):
-    print "# ", i, " ", fibonacci(i)
+    print("# ", i, " ", fibonacci(i))
