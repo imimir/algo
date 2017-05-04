8e12334d410c19b10cd9bda9ae61e1dba720d65e
mimir <scarletfores3@gmail.com>
2017-05-04 22:17:10 +0300

    Add cache support to recursive implementation.

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,18 @@
+def memo(f):
+    cache = {}
+
+    def inner(n):
+        if n not in cache:
+            cache[n] = f(n)
+        return cache[n]
+
+    return inner
+
 def fibonacci(n):
     assert n >= 0
+    return n if n < 2 else fibonacci(n - 1) + fibonacci(n - 2)
 
-    if n < 2:
-        return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-fibonacci(-1)
+fibonacci = memo(fibonacci)
+
 for i in range(100):
     print "# ", i, " ", fibonacci(i)
